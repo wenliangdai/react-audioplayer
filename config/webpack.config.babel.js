5c62cc9c04a61e2499b8583e8a469785060a9af9
psywd2 <psywd2@exmail.nottingham.ac.uk>
2016-12-20 19:04:57 +0000

    move config files into the config folder

--- config/webpack.config.babel.js
+++ config/webpack.config.babel.js
@@ -4,11 +4,12 @@ import devConfig from './webpack.dev.config.babel';
 import prodConfig from './webpack.prod.config.babel';
 
 const CONFIG = process.env.npm_lifecycle_event === 'build' ? prodConfig : devConfig;
+const ROOT_DIR = path.resolve(__dirname, '..');
 
 export default merge({
-  entry: path.join(__dirname, 'src/index.jsx'),
+  entry: path.join(ROOT_DIR, 'src/index.jsx'),
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(ROOT_DIR, 'dist'),
     filename: 'bundle.js',
     publicPath: '/static/',
     library: 'reactAudio',
@@ -25,7 +26,7 @@ export default merge({
       },
       {
         test: /\.css$/,
-        include: [ path.join(__dirname, 'src/globalStyles') ],
+        include: [ path.join(ROOT_DIR, 'src/globalStyles') ],
         loaders: [
           'style-loader',
           'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
@@ -34,8 +35,8 @@ export default merge({
       },
       {
         test: /\.css$/,
-        include: [ path.join(__dirname, 'src') ],
-        exclude: [ path.join(__dirname, 'src/globalStyles') ],
+        include: [ path.join(ROOT_DIR, 'src') ],
+        exclude: [ path.join(ROOT_DIR, 'src/globalStyles') ],
         loaders: [
           'style-loader',
           'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
--- config/webpack.dev.config.babel.js
+++ config/webpack.dev.config.babel.js
@@ -1,17 +1,18 @@
 import path from 'path';
 import webpack from 'webpack';
 
+const ROOT_DIR = path.resolve(__dirname, '..');
 const config = {
   devtool: 'cheap-module-eval-source-map',
   devServer: {
-    contentBase: path.join(__dirname, 'src'),
+    contentBase: path.join(ROOT_DIR, 'src'),
     historyApiFallback: true,
     hot: true,
     inline: true,
     progress: true,
     host: '0.0.0.0',
     port: '8080',
-    stats: { colors: true }
+    stats: 'normal' // customize terminal console info
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
